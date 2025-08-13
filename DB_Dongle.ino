0be02bd008cdb91476d81864ebbf37d500f16d62
alexcrespo98 <138143842+alexcrespo98@users.noreply.github.com>
2025-08-13 09:59:40 -0400

    Create DB_Dongle.ino

---
+++ DB_Dongle.ino
@@ -0,0 +1,47 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#define SERVICE_UUID        "12345678-1234-1234-1234-123456789abc"
+#define CHARACTERISTIC_UUID "87654321-4321-4321-4321-cba987654321"
+
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+
+class MyCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    }
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  BLEDevice::init("ESP32 Dongle");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+  pCharacteristic->addDescriptor(new BLE2902());
+  pService->start();
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+}
+
+void loop() {
+  if (Serial.available() && deviceConnected) {
+    String msg = Serial.readString();
+    pCharacteristic->setValue(msg.c_str());
+    pCharacteristic->notify();
+  }
+}
